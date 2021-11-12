7e7883cc9a94d95b35607549912fd6d77ffb2058
Joshua Opata <opatajoshua@gmail.com>
2021-11-12 14:13:41 +0000

    [TASK] completed person edit and create person fixed

--- app/components/person.js
+++ app/components/person.js
@@ -4,6 +4,7 @@ import { tracked } from '@glimmer/tracking';
 import createPerson from '../gql/mutations/createPerson.gql';
 import updatePerson from '../gql/mutations/updatePerson.gql';
 import { inject as service } from '@ember/service';
+import omit from 'lodash/omit'
 
 export default class PersonComponent extends Component {
   @service
@@ -17,16 +18,28 @@ export default class PersonComponent extends Component {
 
   constructor() {
     super(...arguments);
+    if(this.args.model && this.args.model.id)
+      //omit 'id', '__typename' from the person variable to passed to updatePerson
+      this.person = {...omit(this.args.model, 'id', '__typename')}
+    else
+      // initialize for a new person
+      this.person ={};
   }
 
   @action
-  save() {
-    let variables = this.person;
-    console.log(variables);
-    this.apollo.mutate(createPerson, variables).then((result) => {
-      console.log(result);
-      this.router.transitionTo('people.person', result);
-    });
-    console.log('test');
+  save(event) {
+    event.preventDefault();
+    // edit person
+    if(this.args.model && this.args.model.id){
+      let variables = {id: this.args.model.id, input: this.person};
+      this.apollo.mutate({ mutation:updatePerson, variables }).then((result) => {
+        this.router.transitionTo('people.person', result.updatePerson);
+      });
+    }else{ //new person
+      let variables = { input: this.person};
+      this.apollo.mutate({ mutation:createPerson, variables }).then((result) => {
+        this.router.transitionTo('people.person', result.createPerson);
+      });
+    }
   }
 }
